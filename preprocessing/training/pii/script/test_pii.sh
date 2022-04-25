07158e27de556f66d0decaa95787d98047ab415a
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-04-25 18:33:52 +0200

    Rename path in script to new pathing

--- preprocessing/training/pii/script/test_pii.sh
+++ preprocessing/training/pii/script/test_pii.sh
@@ -1,15 +1,14 @@
 conda activate pii
 
-CATALOGUE_DATA_REPO="/home/lucile/catalogue_data"
-
-cd $CATALOGUE_DATA_REPO
+TRAINING_DATA_PREPARATION_REPO=~/code/big_science/data-preparation/preprocessing/training
+pushd $TRAINING_DATA_PREPARATION_REPO
 
 python pii/pii_processor.py \
     --save-to-json \
-    --dataset-path /home/lucile/data_pii/pre_processing \
+    --dataset-path ~/data_pii/pre_processing \
     --dataset-name lm_eu_oscar.jsonl \
-    --save-path /home/lucile/data_pii/post_processing/lm_eu_oscar.jsonl \
-    --save-check-path /home/lucile/data_pii/post_processing_checks/lm_eu_oscar \
+    --save-path ~/data_pii/post_processing/lm_eu_oscar.jsonl \
+    --save-check-path ~/data_pii/post_processing_checks/lm_eu_oscar \
     --num-proc 3 \
     --batch-size 1000 \
     --save-batch-size 10000 \
