309fb0e919ec8115208b3c09f1d0c29b33817e50
SaulLu <lucilesaul.com@gmail.com>
2022-06-07 23:42:09 +0200

    compute_metadata

--- analysis/lucile/download_ds.py
+++ analysis/lucile/download_ds.py
@@ -2,7 +2,7 @@
 from huggingface_hub import HfApi
 
 hf_api = HfApi()
-author="bigscience-catalogue-lm-data"
+author = "bigscience-catalogue-lm-data"
 list_datasets = hf_api.list_datasets(author=author, use_auth_token=True)
 # %%
 list_datasets
@@ -10,7 +10,7 @@ list_datasets
 len(list_datasets)
 # %%
 with open("list_ds.txt", "w") as f:
-    for item in [ds_info.id[len(f"{author}/"):] for ds_info in list_datasets]:
+    for item in [ds_info.id[len(f"{author}/") :] for ds_info in list_datasets]:
         f.write(f'"{item}"\n')
 #%%
 print(dir(list_datasets[0]))
