1a9f3022df169fceb275c7281f592184bfe90b29
SaulLu <lucilesaul.com@gmail.com>
2022-06-07 23:04:44 +0200

    download ds on jz

---
+++ analysis/lucile/download_ds.py
@@ -0,0 +1,17 @@
+#%%
+from huggingface_hub import HfApi
+
+hf_api = HfApi()
+author="bigscience-catalogue-lm-data"
+list_datasets = hf_api.list_datasets(author=author, use_auth_token=True)
+# %%
+list_datasets
+# %%
+len(list_datasets)
+# %%
+with open("list_ds.txt", "w") as f:
+    for item in [ds_info.id[len(f"{author}/"):] for ds_info in list_datasets]:
+        f.write(f'"{item}"\n')
+#%%
+print(dir(list_datasets[0]))
+# %%
