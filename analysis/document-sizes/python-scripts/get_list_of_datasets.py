9f17684c917cb31e62431604b3b7c8573b67b9c8
SaulLu <lucilesaul.com@gmail.com>
2022-06-13 11:13:03 +0000

    update paths

--- analysis/document-sizes/python-scripts/get_list_of_datasets.py
+++ analysis/document-sizes/python-scripts/get_list_of_datasets.py
@@ -1,17 +1,12 @@
-#%%
 from huggingface_hub import HfApi
 
 hf_api = HfApi()
 author = "bigscience-catalogue-lm-data"
 list_datasets = hf_api.list_datasets(author=author, use_auth_token=True)
-# %%
-list_datasets
-# %%
-len(list_datasets)
-# %%
+print("Number of datasets", len(list_datasets))
+
 with open("list_ds.txt", "w") as f:
     for item in [ds_info.id[len(f"{author}/") :] for ds_info in list_datasets]:
         f.write(f'"{item}"\n')
-#%%
+
 print(dir(list_datasets[0]))
-# %%
